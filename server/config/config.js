07ad3dff2fd87a6386909c8534e3ba15306769d2
ericruizm <ericruizmolero@gmail.com>
2020-05-15 16:34:01 +0200

    Google sign-in

--- server/config/config.js
+++ server/config/config.js
@@ -36,3 +36,11 @@ if (process.env.NODE_ENV === "dev") {
 }
 
 process.env.URLDB = urlDB;
+
+// ======================
+// Google Client ID
+// ======================
+
+process.env.CLIENT_ID =
+    process.env.CLIENT_ID ||
+    "593234316154-u2gbv2lo1iidcm7n1emf5h44pq9s490q.apps.googleusercontent.com";
